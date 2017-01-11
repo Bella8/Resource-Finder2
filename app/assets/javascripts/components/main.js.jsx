1c46d69eb786afbbd9ccf23803a8524609d9ce73
Beylul <bkbreab8@gmail.com>
2017-01-10 18:11:03 -0800

    submit and on click handlers for the form are working

--- app/assets/javascripts/components/main.js.jsx
+++ app/assets/javascripts/components/main.js.jsx
@@ -1,15 +1,36 @@
 var Main = React.createClass({
-  render() {
+
+  handleFormSubmit: function (formSubmitEvent) {
+  formSubmitEvent.preventDefault();
+  var testtwo = this.refs.testmain.state;
+
+
+  console.log('You have selected:', testtwo);
+},
+
+  render: function() {
     var answers = [	"Homeless/Live in a shelter",
-     "transitional housing", "Renting", "Other"];
+    "transitional housing", "Renting", "Other"];
+    var answersYN = [	"Yes",
+    "No"];
+    var answersThree = ["Employed","Underemployed", "Out of work – looking for work", "Out of work – not currently looking for work"]
 
     return(
       <div>
-        <Header />
-        <form><Question question="Client current housing type?
-          " answers = {answers} name= "questionOne"/></form>
+        <form onSubmit={this.handleFormSubmit}>
+          <Question ref="testmain" question="Client current housing type?
+          " answers = {answers} name= "questionOne"/>
+        <input
+          type="submit"
+          className="btn btn-primary float-right"
+          value="Submit"/>
 
-      </div>
-    )
-  }
+        <button
+          className="btn btn-link float-left"
+          onClick={this.handleClearForm}>Clear form</button>
+      </form>
+
+    </div>
+  )
+}
 });
--- app/assets/javascripts/components/question.js.jsx
+++ app/assets/javascripts/components/question.js.jsx
@@ -2,15 +2,23 @@ var Question = React.createClass({
   propTypes:{
     name: React.PropTypes.string,
     question: React.PropTypes.string,
-    answers: React.PropTypes.arrayOf(React.PropTypes.string)
+    answers: React.PropTypes.arrayOf(React.PropTypes.string),
+    value: React.PropTypes.string
   },
 
-  render() {
+  handleChange: function (event) {
+      this.setState({value: event.target.value});
+      console.log(event.target.value);
+    },
+
+  render: function() {
     var props = this.props;
+    var change = this.handleChange;
+
     var answerElements = this.props.answers.map(function(answer, i){
       return(
 
-        <li key={i}><input type="radio" name={props.name} value="" id={i}  /> <label>{answer}</label></li>
+        <li key={i}><input  onClick={change} type="radio" name={props.name} value={answer} ref="test" id={i}  /> <label>{answer}</label></li>
 
         )
       });
