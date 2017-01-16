1b090cc5c3b6d2dd48ef37faed860441a567d96c
Beylul <bkbreab8@gmail.com>
2017-01-16 10:15:13 -0800

    More cleaning up

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,30 @@
 class SiteController < ApplicationController
   def index
-     @items = Item.all
+    @items = Item.all
   end
 
+  def mainpage
+  end
+
+  def from
+  end
+
+  def allitems
+  end
+
+  def search
+    @client_info = Item.search(params[:query])
+    if request.xhr?
+      render :json => @client_info.to_json
+    else
+      render :index
+    end
+  end
+
+  def edit
+  end
+
+  def delete
+  end
 
 end
